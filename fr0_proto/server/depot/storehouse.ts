c58480242e5c3a3d2af38a7c795124778fc30853
yahiro <yahiro07@users.noreply.github.com>
2024-01-14 19:54:00 +0900

    mongodbのパッケージを変更

--- fr0_proto/server/depot/storehouse.ts
+++ fr0_proto/server/depot/storehouse.ts
@@ -8,17 +8,22 @@ async function createStoreHouse() {
   if (!mongoSpec) raiseError("env variable MONGO_SPEC undefined");
   const [uri, dbName] = mongoSpec.split("|");
   if (!dbName) raiseError("invalid MONGO_SPEC format.");
-  const client = new MongoClient(uri);
-  await client.connect();
+  const client = new MongoClient();
+  await client.connect(uri);
   console.log("connected to db");
-  const db = client.db(dbName);
+  const db = client.database(dbName);
 
   const colProject = db.collection<ProjectEntity>("project");
   const projectCabinet = createMongoGeneralCabinet(colProject, "projectId");
 
-  await colProject.createIndex({ projectId: -1 }, { unique: true });
+  // await colProject.createIndex({ projectId: -1 }, { unique: true });
+  await colProject.createIndexes({
+    indexes: [
+      { key: { projectId: 1 }, name: "project_id", unique: true },
+    ],
+  });
 
   return { colProject, projectCabinet };
 }
 
-export const storeHouse = await createStoreHouse();
+export const storehouse = await createStoreHouse();
