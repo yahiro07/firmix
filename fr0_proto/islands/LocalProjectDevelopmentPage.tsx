77143e5edf055a1c74f2ff466cbcd92734ca6871
yahiro <yahiro07@users.noreply.github.com>
2024-01-16 01:57:25 +0900

    フォルダ読み込みのガイドUIを表示

--- fr0_proto/islands/LocalProjectDevelopmentPage.tsx
+++ fr0_proto/islands/LocalProjectDevelopmentPage.tsx
@@ -1,33 +1,34 @@
 import { useMemo } from "preact/hooks";
 import { useReasyState } from "~/aux/reasy/reasy_state_local.ts";
+import { css } from "~/aux/resin/resin_css.ts";
 import { downloadBinaryFileBlob } from "~/aux/utils_fe/downloading_link.ts";
-import { ConfigurationEditItem } from "~/base/types_project_edit.ts";
-import { firmixPresenter } from "~/cathedral/firmix_presenter/mod.ts";
-
 import {
   LocalDevelopmentWork,
   LocalDevelopmentWork_Loaded,
 } from "~/base/types_local_project.ts";
+import { ConfigurationEditItem } from "~/base/types_project_edit.ts";
+import { firmixPresenter } from "~/cathedral/firmix_presenter/mod.ts";
 import { firmixWorkBuilder } from "~/cathedral/firmix_work/mod.ts";
 import { rpcClient } from "~/common/rpc_client.ts";
+import { flexCentered, flexVertical } from "~/common/utility_styles.ts";
+import { IconIconify } from "~/components/IconIconify.tsx";
 import { LocalProjectAssetsArea } from "~/features/local_project/LocalProjectAssetsArea.tsx";
 import { LocalProjectLoadingArea } from "~/features/local_project/LocalProjectLoadingArea.tsx";
 import { ParametersConfigurationArea } from "~/features/project/ParametersConfigurationArea.tsx";
 
 export default function LocalProjectDevelopmentPage() {
   const [{ work }, { setWork }] = useReasyState({
-    work: undefined as (LocalDevelopmentWork | undefined),
+    work: undefined as LocalDevelopmentWork | undefined,
   });
 
-  const project = work?.state === "loaded" && work.project ||
-    undefined;
-  const errorMessage = work?.state === "error" && work.message || undefined;
+  const project = (work?.state === "loaded" && work.project) || undefined;
+  const errorMessage = (work?.state === "error" && work.message) || undefined;
 
   const submitEditItems = (editItems: ConfigurationEditItem[]) => {
     if (!project) return;
     const modFirmware = firmixPresenter.patchLocalProjectFirmware(
       project,
-      editItems,
+      editItems
     );
     downloadBinaryFileBlob(modFirmware.fileName, modFirmware.binaryBytes);
   };
@@ -36,20 +37,24 @@ export default function LocalProjectDevelopmentPage() {
     if (!project) return;
     const modFirmware = firmixPresenter.patchLocalProjectFirmware(
       project,
-      editItems,
+      editItems
     );
     const newWork = await firmixWorkBuilder.workEmitModifiedFirmware(
       work as LocalDevelopmentWork_Loaded,
-      modFirmware,
+      modFirmware
     );
     setWork(newWork);
   };
 
-  const configurationsSourceItems = useMemo(() =>
-    project &&
-      firmixPresenter.buildConfigurationSourceItems(
-        project.patchingManifest,
-      ) || undefined, [project]);
+  const configurationsSourceItems = useMemo(
+    () =>
+      (project &&
+        firmixPresenter.buildConfigurationSourceItems(
+          project.patchingManifest
+        )) ||
+      undefined,
+    [project]
+  );
 
   const handleSubmit = async () => {
     if (!project) return;
@@ -58,7 +63,7 @@ export default function LocalProjectDevelopmentPage() {
   };
 
   return (
-    <div>
+    <div q={style}>
       <LocalProjectLoadingArea setWork={setWork} />
       <div>
         <LocalProjectAssetsArea project={project!} if={project} />
@@ -72,7 +77,33 @@ export default function LocalProjectDevelopmentPage() {
         />
         <div if={errorMessage}>{errorMessage}</div>
       </div>
-      <button onClick={handleSubmit} disabled={!project}>投稿</button>
+      <button onClick={handleSubmit} disabled={!project}>
+        投稿
+      </button>
+      <div q="blank-filler" if={!work}>
+        <IconIconify spec="ph:folder-thin" q="folder-icon" />
+        <div q="text">
+          ローカルプロジェクトのフォルダを
+          <br />
+          ドラッグ&ドロップして読み込みます
+        </div>
+      </div>
     </div>
   );
 }
+
+const style = css`
+  height: 100%;
+  ${flexVertical()};
+  > .blank-filler {
+    flex-grow: 1;
+    ${flexCentered()};
+    flex-direction: column;
+    > .folder-icon {
+      font-size: 70px;
+    }
+    > .text {
+      text-align: center;
+    }
+  }
+`;
