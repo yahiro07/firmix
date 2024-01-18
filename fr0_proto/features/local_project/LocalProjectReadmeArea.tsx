5705e7418e23369881394f07ed4edc11c494bf62
yahiro <yahiro07@users.noreply.github.com>
2024-01-19 06:06:31 +0900

    readme表示でのサムネイルの埋め込みを無効化

--- fr0_proto/features/local_project/LocalProjectReadmeArea.tsx
+++ fr0_proto/features/local_project/LocalProjectReadmeArea.tsx
@@ -19,12 +19,19 @@ export const LocalProjectReadmeArea = createFC<Props>(({ project }) => {
     const htmlCode = render(readmeFileContent, {
       baseUrl: sourceCodeUrl ? sourceCodeUrl + "/" : "",
     });
-    const repositoryThumbnailUrl = `${sourceCodeUrl}/${thumbnailImageFileName}`;
-    const modHtmlCode = htmlCode.replaceAll(
-      repositoryThumbnailUrl,
-      thumbnailImageDataUrl
-    );
-    return modHtmlCode;
+    if (0) {
+      //readme内に含まれるサムネイル画像への相対参照をプロジェクトリソースの画像で置き換える対応
+      //readme内にサムネイル以外の他の画像を置いたときにこれらが表示されず混乱する可能性があるので、
+      //一旦無効にしておく
+      const repositoryThumbnailUrl = `${sourceCodeUrl}/${thumbnailImageFileName}`;
+      const modHtmlCode = htmlCode.replaceAll(
+        repositoryThumbnailUrl,
+        thumbnailImageDataUrl
+      );
+      return modHtmlCode;
+    } else {
+      return htmlCode;
+    }
   }, [project]);
 
   return (
