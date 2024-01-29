4ecb0eee54f9ae33f5e7a52b6403678d233c66ee
yahiro <yahiro07@users.noreply.github.com>
2024-01-29 09:23:15 +0900

    preconnectを追加

--- fr0_proto/routes/_app.tsx
+++ fr0_proto/routes/_app.tsx
@@ -10,12 +10,18 @@ export default async function App(_req: Request, ctx: FreshContext) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Firmix</title>
-        <link rel="stylesheet" href="/styles.css" />
         <ResinCssGlobalStyle css={globalStyle} />
         <ResinCssEmitter />
         <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=M+PLUS+1p&family=M+PLUS+2&family=Murecho&family=Sawarabi+Gothic&display=swap"
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=M+PLUS+2&display=swap"
           rel="stylesheet"
         />
       </head>
