c41910eeff203a1e056ab6027fc26bcaae83ac5b
yahiro <yahiro07@users.noreply.github.com>
2024-02-01 12:49:25 +0900

    サンプルプロジェクトを削除

--- fr0_example_projects/proj1/upload_project.sh
+++
@@ -1,5 +0,0 @@
-curl -X POST -H "Authorization: token mytoken_aabbccdd" \
-  -F readme=@./readme.md -F metadata=@./project.fm1.json \
-  -F thumbnail=@./thumbnail.jpg -F firmware=@./.pio/build/generic/firmware.uf2 \
-  https://firmix.deno.dev/api/project/upload
-
