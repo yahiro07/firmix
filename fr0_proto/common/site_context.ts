c19acd79aa41e2d5d8e66ebeed78e343d68728fd
yahiro <yahiro07@users.noreply.github.com>
2024-03-19 04:18:49 +0900

    アクティブターゲットを表示

--- fr0_proto/common/site_context.ts
+++ fr0_proto/common/site_context.ts
@@ -1,10 +1,12 @@
 import { createContext } from "preact";
 import { useContext } from "preact/hooks";
+import { CoactiveState } from "~/base/types_dto.ts";
 import { LoginUser } from "~/base/types_dto_internal.ts";
 
 export type SiteContextValue = {
   pagePath: string;
   loginUser: LoginUser | undefined;
+  coactiveState: CoactiveState;
 };
 
 export const siteContext = createContext<SiteContextValue>({} as any);
--- fr0_proto/features/project_list/HomeTargetSelectionBar.tsx
+++ fr0_proto/features/project_list/HomeTargetSelectionBar.tsx
@@ -2,20 +2,35 @@ import { createFCX, css } from "resin";
 import { ProjectRealm } from "~/base/types_app_common.ts";
 import { coactiveStateWriter } from "~/common/coactive_state_writer.ts";
 import { uiStyleClickable } from "~/common/common_styles.ts";
+import { useSiteContext } from "~/common/site_context.ts";
 import { uiSystem } from "~/common/ui_system.ts";
 import { flexAligned } from "~/common/utility_styles.ts";
 
 export const HomeTargetSelectionBar = createFCX(
   () => {
+    const { coactiveState } = useSiteContext();
+
     const handleSelect = (realm: ProjectRealm) => {
       coactiveStateWriter.setValue({ homeTargetRealm: realm });
       uiSystem.reload();
     };
 
+    const realm = coactiveState.homeTargetRealm;
+
     return (
       <div>
-        <div onClick={() => handleSelect("general")}>一般</div>
-        <div onClick={() => handleSelect("keyboard")}>キーボード</div>
+        <div
+          q={realm === "general" && "--active"}
+          onClick={() => handleSelect("general")}
+        >
+          電子工作
+        </div>
+        <div
+          q={realm === "keyboard" && "--active"}
+          onClick={() => handleSelect("keyboard")}
+        >
+          キーボード
+        </div>
       </div>
     );
   },
@@ -23,6 +38,9 @@ export const HomeTargetSelectionBar = createFCX(
     ${flexAligned(16)};
     > div {
       ${uiStyleClickable};
+      &.--active {
+        font-weight: bold;
+      }
     }
   `
 );
