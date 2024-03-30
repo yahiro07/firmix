ef443d9c9569798523c08a890db4097e2e322323
yahiro <yahiro07@users.noreply.github.com>
2024-03-30 18:24:14 +0900

    targetMCUの定義を拡張

--- fr0_proto/cardinal/firmix_core_firmware_patching/matada_input_validator.ts
+++ fr0_proto/cardinal/firmix_core_firmware_patching/matada_input_validator.ts
@@ -7,13 +7,22 @@ const customDataItemCommon = {
   required: z.boolean().optional(),
 };
 
+const stringLiterals = (literals: string[]) =>
+  z.union(literals.map((lit) => z.literal(lit)) as any);
+
 const schemaMetadataFileContent = z.object({
   projectGuid: z.string().uuid(),
   projectName: z.string().max(32),
   parentProjectGuid: z.string().uuid().optional(),
   variationName: z.string().max(32).optional(),
   introductionLines: z.array(z.string().max(256)),
-  targetMcu: z.literal("RP2040"),
+  targetMcu: stringLiterals([
+    "RP2040",
+    "SAMD21",
+    "SAMD51",
+    "nRF52840",
+    "ESP32S3",
+  ]),
   primaryTargetBoard: z.string().max(32),
   realm: z.union([z.literal("general"), z.literal("keyboard")]),
   tags: z.array(z.string().max(32)),
