5c1ec9d865cb938953d8c388ee76e73317a1c9b6
yahiro <yahiro07@users.noreply.github.com>
2024-03-31 10:43:59 +0900

    バージョンを更新

--- fr0_proto/base/app_config.ts
+++ fr0_proto/base/app_config.ts
@@ -1,5 +1,5 @@
 export const appConfig = {
-  versionCode: "240331c",
+  versionCode: "240331d",
   isDevelopment:
     typeof Deno !== "undefined"
       ? Deno.env.get("ENV_TYPE") === "development"
