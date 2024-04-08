1e2a07be24ad30de53e86dc146adfa35376d3e4c
yahiro <yahiro07@users.noreply.github.com>
2024-04-09 06:01:09 +0900

    コーディングミスを修正

--- firmix_node_remix/app/auxiliaries/base_env_adapters/crypto.ts
+++ firmix_node_remix/app/auxiliaries/base_env_adapters/crypto.ts
@@ -1,7 +1,7 @@
 import crypto from "crypto";
 
 export function generateHashMd5(input: string | Uint8Array) {
-  return crypto.createHash("md5").update(input).toString();
+  return crypto.createHash("md5").update(input).digest("hex");
 }
 
 export function makeHmacSha256(text: string, secret: string) {
