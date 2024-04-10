da9b96732f5a528b2e3fb83575bc77a4782427fa
yahiro <yahiro07@users.noreply.github.com>
2024-04-11 06:34:54 +0900

    色定義にcss variableを使用

--- firmix_node_remix/packages/auxiliaries/utils_fe_react/fcs.ts
+++ firmix_node_remix/packages/auxiliaries/utils_fe_react/fcs.ts
@@ -2,8 +2,9 @@ import { FC } from "react";
 
 export function createFCS<TProps, TStyles>(
   baseFn: (props: TProps, styles: TStyles) => JSX.Element,
-  styles: TStyles
+  styles?: TStyles
 ): FC<TProps> {
+  if (!styles) return baseFn;
   return (props: TProps) => {
     return baseFn(props, styles);
   };
