fee2f00b760c3635670af58f16cdacabe6ce742b
yahiro <yahiro07@users.noreply.github.com>
2024-04-12 03:44:04 +0900

    fcxを使用

--- firmix_node_remix/packages/site-firmix-base/app/features/local_project/LocalProjectAssetsArea.tsx
+++ firmix_node_remix/packages/site-firmix-base/app/features/local_project/LocalProjectAssetsArea.tsx
@@ -1,6 +1,5 @@
 import { css } from "@linaria/core";
-import { createFC } from "auxiliaries/utils_fe_react/create_fc";
-import { domStyled } from "auxiliaries/utils_fe_react/fcx";
+import { createFCX } from "auxiliaries/utils_fe_react/fcx";
 import { flexAligned } from "shared/common/utility_styles.ts";
 import { IconIconifyZ } from "shared/components/IconIconifyZ.tsx";
 import {
@@ -13,58 +12,60 @@ type Props = {
   project: LocalDevelopmentProject;
 };
 
-const AssetEntry = createFC<{
+const AssetEntry = createFCX<{
   title: string;
   asset: LocalAssetBase;
   infoAdditional?: string;
-}>(({ title, asset, infoAdditional }) => {
-  const iconSpec = (
-    {
-      valid: "mdi:check",
-      warning: "mdi:warning-outline",
-      error: "subway:error",
-    } as const
-  )[asset.validity];
+}>(
+  ({ title, asset, infoAdditional }) => {
+    const iconSpec = (
+      {
+        valid: "mdi:check",
+        warning: "mdi:warning-outline",
+        error: "subway:error",
+      } as const
+    )[asset.validity];
 
-  return domStyled(
-    <div>
-      <div q="ae-heading">
-        <IconIconifyZ
-          spec={iconSpec}
-          q={["validity-icon", `--validity-${asset.validity}`]}
-        />
-        <span>
-          {title}: {asset.filePath} {infoAdditional}
-        </span>
+    return (
+      <div>
+        <div q="ae-heading">
+          <IconIconifyZ
+            spec={iconSpec}
+            q={["validity-icon", `--validity-${asset.validity}`]}
+          />
+          <span>
+            {title}: {asset.filePath} {infoAdditional}
+          </span>
+        </div>
+        <div q="errors">
+          {asset.errorLines.map((line, index) => (
+            <div key={index}>{line}</div>
+          ))}
+        </div>
       </div>
-      <div q="errors">
-        {asset.errorLines.map((line, index) => (
-          <div key={index}>{line}</div>
-        ))}
-      </div>
-    </div>,
-    css`
-      > .ae-heading {
-        ${flexAligned(2)};
+    );
+  },
+  css`
+    > .ae-heading {
+      ${flexAligned(2)};
 
-        > .validity-icon {
-          &.--validity-valid {
-            color: green;
-          }
-          &.--validity-warning {
-            color: orange;
-          }
-          &.--validity-error {
-            color: red;
-          }
+      > .validity-icon {
+        &.--validity-valid {
+          color: green;
+        }
+        &.--validity-warning {
+          color: orange;
+        }
+        &.--validity-error {
+          color: red;
         }
       }
-      > .errors {
-        margin-left: 16px;
-      }
-    `
-  );
-});
+    }
+    > .errors {
+      margin-left: 16px;
+    }
+  `
+);
 
 const local = {
   extractThumbnailInfoAdditional(assetThumbnail: LocalAsset_Thumbnail): string {
@@ -76,66 +77,67 @@ const local = {
   },
 };
 
-export const LocalProjectAssetsArea = createFC<Props>(({ project }) => {
-  const {
-    assetReadme,
-    assetMetadata,
-    assetThumbnail,
-    assetFirmware,
-    modFirmwareFilePath,
-  } = project;
-  const thumbnailInfoAdditional =
-    local.extractThumbnailInfoAdditional(assetThumbnail);
-  const thumbnailUrl = assetThumbnail.thumbnailContainer?.imageDataUrl;
+export const LocalProjectAssetsArea = createFCX<Props>(
+  ({ project }) => {
+    const {
+      assetReadme,
+      assetMetadata,
+      assetThumbnail,
+      assetFirmware,
+      modFirmwareFilePath,
+    } = project;
+    const thumbnailInfoAdditional =
+      local.extractThumbnailInfoAdditional(assetThumbnail);
+    const thumbnailUrl = assetThumbnail.thumbnailContainer?.imageDataUrl;
 
-  return (
-    <div q={style}>
-      <h3>
-        <IconIconifyZ spec="ph:files" q="icon" />
-        <span>プロジェクトリソース</span>
-      </h3>
-      {/* <div>ターゲットMCU:{patchingManifest.targetMcu}</div> */}
-      <AssetEntry title="Readmeファイル" asset={assetReadme} />
-      <AssetEntry title="メタデータファイル" asset={assetMetadata} />
-      <AssetEntry
-        title="サムネイル"
-        asset={assetThumbnail}
-        infoAdditional={thumbnailInfoAdditional}
-      />
-      <AssetEntry title="ファームウェア" asset={assetFirmware} />
-      <div if={modFirmwareFilePath}>
-        パッチ適用済ファームウェア: {modFirmwareFilePath}
+    return (
+      <div>
+        <h3>
+          <IconIconifyZ spec="ph:files" q="icon" />
+          <span>プロジェクトリソース</span>
+        </h3>
+        {/* <div>ターゲットMCU:{patchingManifest.targetMcu}</div> */}
+        <AssetEntry title="Readmeファイル" asset={assetReadme} />
+        <AssetEntry title="メタデータファイル" asset={assetMetadata} />
+        <AssetEntry
+          title="サムネイル"
+          asset={assetThumbnail}
+          infoAdditional={thumbnailInfoAdditional}
+        />
+        <AssetEntry title="ファームウェア" asset={assetFirmware} />
+        <div if={modFirmwareFilePath}>
+          パッチ適用済ファームウェア: {modFirmwareFilePath}
+        </div>
+        <div q="thumbnail-box" if={thumbnailUrl}>
+          <img src={thumbnailUrl} alt="thumbnail" />
+        </div>
       </div>
-      <div q="thumbnail-box" if={thumbnailUrl}>
-        <img src={thumbnailUrl} alt="thumbnail" />
-      </div>
-    </div>
-  );
-});
+    );
+  },
+  css`
+    padding: 8px;
+    position: relative;
 
-const style = css`
-  padding: 8px;
-  position: relative;
-
-  > h3 {
-    ${flexAligned()};
-    font-size: 20px;
-    > .icon {
-      font-size: 24px;
+    > h3 {
+      ${flexAligned()};
+      font-size: 20px;
+      > .icon {
+        font-size: 24px;
+      }
     }
-  }
-  > .thumbnail-box {
-    position: absolute;
-    right: 0;
-    top: 0;
-    margin: 8px;
-    width: 160px;
-    height: 120px;
+    > .thumbnail-box {
+      position: absolute;
+      right: 0;
+      top: 0;
+      margin: 8px;
+      width: 160px;
+      height: 120px;
 
-    > img {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
+      > img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
     }
-  }
-`;
+  `
+);
