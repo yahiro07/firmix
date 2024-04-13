cddc533f962703411e4b2da63bfbc9e41266ac92
yahiro <yahiro07@users.noreply.github.com>
2024-04-14 02:20:54 +0900

    環境変数の名前を変更

--- firmix_node_remix/packages/web-firmix/app/central/domain_helpers/project_helper.ts
+++ firmix_node_remix/packages/web-firmix/app/central/domain_helpers/project_helper.ts
@@ -3,13 +3,13 @@ import { getEnvVariable } from "web-firmix/app/central/base/envs";
 
 export const projectHelper = {
   getThumbnailImageUrl(project: ProjectEntity): string {
-    const r2PublicUrl = getEnvVariable("R2_PUBLIC_URL");
+    const r3PublicUrl = getEnvVariable("S3_PUBLIC_URL");
     const { projectId, thumbnailFileName, thumbnailRevision } = project;
-    return `${r2PublicUrl}/${projectId}/${thumbnailFileName}?rev=${thumbnailRevision}`;
+    return `${r3PublicUrl}/${projectId}/${thumbnailFileName}?rev=${thumbnailRevision}`;
   },
   getFirmwareBinaryUrl(project: ProjectEntity): string {
-    const r2PublicUrl = getEnvVariable("R2_PUBLIC_URL");
+    const r3PublicUrl = getEnvVariable("S3_PUBLIC_URL");
     const { projectId, firmwareFileName, firmwareRevision } = project;
-    return `${r2PublicUrl}/${projectId}/${firmwareFileName}?rev=${firmwareRevision}`;
+    return `${r3PublicUrl}/${projectId}/${firmwareFileName}?rev=${firmwareRevision}`;
   },
 };
