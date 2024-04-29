561da1a1b6d5c6952a09b9de2bfd39f02bc14fc0
yahiro <yahiro07@users.noreply.github.com>
2024-04-29 18:27:57 +0900

    リクエストの対象URLの取得を修正

--- firmix_nodejs/packages/web-firmix-nextjs/src/app/auth/github/route.tsx
+++ firmix_nodejs/packages/web-firmix-nextjs/src/app/auth/github/route.tsx
@@ -1,17 +1,17 @@
 import { oauthClientGithub } from "@mx/web-firmix-nextjs/src/central/user_auth/oauth_clients";
 import {
   createGetHandler,
-  getRequestSourceUrl_NextJS,
+  getRequestSourceUrl,
   responseRedirect,
 } from "../../route_helper";
 
 export const GET = createGetHandler(({ request }) => {
-  const reqUrl = getRequestSourceUrl_NextJS(request);
+  const reqUrl = getRequestSourceUrl(request);
   const url = oauthClientGithub.getAuthUrl(reqUrl);
   console.log(`auth github`, {
     reqUrl,
     destUrl: url,
-    headers: [...request.headers.entries()].map((k, v) => `${k}:${v}`),
+    headers: [...request.headers.entries()].map(([k, v]) => `${k}: ${v}`),
   });
   return responseRedirect(url);
 });
